d594bb59056035b60d9a30dd318893c6ae671b36
hope <nadezdaturbina00511@gmail.com>
2022-11-18 03:11:52 +0300

    мяяяяу

--- Data.sql
+++ Data.sql
@@ -75,7 +75,7 @@ VALUES
        ('Молочные продукты'),
        ('Яйца'),
        ('Морепродукты'),
-       ('Мясо')
+       ('Мясо');
 
 INSERT INTO UnitMeasures (Name)
 VALUES
@@ -86,7 +86,7 @@ VALUES
        ('ст. л'),
        ('ч. л'),
        ('ст'),
-       ('по вкусу')
+       ('по вкусу');
 
 INSERT INTO Ingredients (Name, TypeId, UnitMeasureID)
 VALUES
@@ -94,29 +94,35 @@ VALUES
        ('Хлеб', 4, 1),
        ('Чеснок', 6, 8),
        ('Сливочное масло', 10, 2),
-       ('Растительное масло', 8, 8)
+       ('Растительное масло', 8, 8);
 
 INSERT INTO Cuisines (Name)
 VALUES
-       ('Домашняя')
+       ('Домашняя');
 
 INSERT INTO MatrixFavorite (RecipesID)
 VALUES
        ('0;0;0'),
-       ('0;0;1')
+       ('0;0;1');
 
 INSERT INTO MatrixFridge (IngredientsID)
 VALUES
        ('0;0;0;0;0'),
-       ('0;0;0;0;0')
+       ('0;0;0;0;0');
 
 INSERT INTO MatrixIngredients (IngredientsID)
 VALUES
        ('30;1;0;0;1'),
        ('30;1;10;0;1'),
-       ('0;1;0;1;1')
+       ('0;1;0;1;1');
 
 INSERT INTO Users (UserName, UserPassword)
 VALUES
        ('admin', 'adminadmin'),
-       ('Gleb', 'secretpassword')
+       ('Gleb', 'secretpassword');
+
+INSERT INTO Recipes (Name, Information, CuisineID, CountSteps, Steps)
+VALUES
+       ('Тост с сыром', 'Хрустящий хлеб с расплавленным сыром', 1, 2, 'Пожарить хлеб с двух сторон до хрустящей корочки;Положить на хлеб сыр'),
+       ('Тост с маслом и сыром', 'Хрустящий хлеб с маслом и расплавленным сыром', 1, 3, 'Пожарить хлеб с двух сторон до хрустящей корочки;Намазать хлеб маслом;Положить на хлеб сыр''),
+       (''Тост с чесноком'', ''Хрустящий хлеб с ароматом чеснока'', 1, , ''Пожарить хлеб с двух сторон до хрустящей корочки;Натереть хлеб отчищенным чесноком');
